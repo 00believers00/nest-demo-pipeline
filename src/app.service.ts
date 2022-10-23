eab5d117b807b675dd37ddbbd204d0403d315b82
Bank Promarin <np.nawin@gmail.com>
2022-10-23 16:19:15 +0700

    test change and edit name jobs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Pipeline OK';
+    return 'Hello World! Pipeline OK Edit pipeline 1';
   }
 }
