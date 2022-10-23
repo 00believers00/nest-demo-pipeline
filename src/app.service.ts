8eabfd240f5cd11a3c3872ac8957b9dd791462d2
Bank Promarin <np.nawin@gmail.com>
2022-10-23 18:41:00 +0700

    update pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Pipeline OK Edit pipeline 4';
+    return 'Hello World! Pipeline OK Edit pipeline 5';
   }
 }
